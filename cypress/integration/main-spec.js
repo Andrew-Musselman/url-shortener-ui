6ccd8dc743bb5a575d637429f982223ebeb4eed4
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-26 10:33:00 -0600

    Add first test to test all aspects of the page

--- cypress/integration/main-spec.js
+++ cypress/integration/main-spec.js
@@ -3,7 +3,12 @@ describe('main', () => {
         cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
             status: 200,
             fixture: 'first-get'
-        })
+        }).as('First Stub')
         cy.visit("http://localhost:3000")
+        .get('h1').should('contain', 'URL Shortener').should('be.visible')
+        .get('div[class="url"]').should('be.visible')
+        .get('h3').should('contain', 'Awesome photo').should('be.visible')
+        .get('a').should('contain', 'http://localhost:3001/useshorturl/1').should('be.visible')
+        .get('p').should('contain', 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80').should('be.visible')
     })
 })
