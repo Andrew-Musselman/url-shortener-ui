e4031d431c7d195a01a1e2af426244a107aa4762
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-26 10:25:00 -0600

    Set up intercept

--- cypress/integration/main-spec.js
+++ cypress/integration/main-spec.js
@@ -1,5 +1,9 @@
 describe('main', () => {
     it('As a user, when I visit the page I should see the title and any existing shortend URLS', () => {
-        
+        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+            status: 200,
+            fixture: 'first-get'
+        })
+        cy.visit("http://localhost:3000")
     })
 })
