66037fbbab9cc06271b3b78639a712365d98fad8
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-26 10:48:25 -0600

    Add third test testing the input fields ability to hold value

--- cypress/integration/main-spec.js
+++ cypress/integration/main-spec.js
@@ -16,8 +16,18 @@ describe('main', () => {
             status: 200,
             fixture: 'first-get'
         }).as('First Stub')
+        cy.visit("http://localhost:3000")
         .get('input[name="title"]').should('have.attr', 'placeholder', 'Title...').should('be.visible')
         .get('input[name="urlToShorten"]').should('have.attr', 'placeholder', 'URL to Shorten...').should('be.visible')
         .get('button').should('be.visible')
+    })
+    it('As a user, as I type in the input fields they should hold the value of what I type', () => {
+        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+            status: 200,
+            fixture: 'first-get'
+        }).as('First Stub')
+        cy.visit("http://localhost:3000")
+        .get('input[name="title"]').type('New URL').should('have.value', 'New URL')
+        .get('input[name="urlToShorten"]').type('https://example.com/a-new-url/10345886-985663').should('have.value', 'https://example.com/a-new-url/10345886-985663')
     })
 })
