46380a3a798ee3b40e820e8db369494f185d5b67
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-26 09:52:15 -0600

    Add submit new URL method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,16 +12,33 @@ export class App extends Component {
     }
   }
 
-  componentDidMount() {
+  initializeURLs = () => {
     getUrls().then(data => this.setState({urls: data.urls}))
   }
+
+  submitNewURL = (something, something_else) => {
+    let body = {
+      title: something,
+      long_url: something_else
+    }
+    fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(body)
+    }).then(() => this.initializeURLs())
+  }
+
+  componentDidMount() {
+    this.initializeURLs()
+  }
+    
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm submitNewURL={this.submitNewURL}/>
         </header>
 
         <UrlContainer urls={this.state.urls}/>
